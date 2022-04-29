eac96d807fafd1b1e9239add2c39dfafa8c27729
yuto ichinose <yuto.0526.sano@gmail.com>
2022-04-29 14:53:19 +0900

    NewsAPIとaxiosでニュース取得機能実装

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
-import React from "react";
-import { StyleSheet, View, } from "react-native";
+import React, { useEffect, useState } from "react";
+import { FlatList, StyleSheet, View, } from "react-native";
 import NewsArticle from "./components/NewsArticle";
 import Counter from "./components/Counter";
+import Constants from "expo-constants";
+import axios from "axios";
+
+const APIKey = Constants.manifest.extra.newsApiKey
+const URI = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${APIKey}`
 
 export default function App() {
+  const [news,setNews] = useState([])
+  useEffect(()=>{
+    getNews()
+  },[])
+
+  const getNews = async () => {
+    const response = await axios.get(URI)
+    setNews(response.data.articles);
+  }
   return (
     <View style={styles.container}>
-      <NewsArticle imageuri = 'https://picsum.photos/id/237/200/300' title = 'Hello My News' subText = 'ようこそ！！'/>
-      <NewsArticle imageuri = './images/app.png' title = 'Hello My News' subText = 'ようこそ！！'/>
-      <Counter/>
+      <FlatList
+        data = {news}
+        renderItem = {({item}) => (
+          <NewsArticle 
+            imageuri = {item.urlToImage} 
+            title = {item.title} 
+            subText = {item.publishedAt}
+          />
+        )}
+        keyExtractor = {(item, index) => index.toString()}
+      />
     </View>
   );
 }
@@ -17,7 +39,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
