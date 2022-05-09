24229d43a3a9f3c4e12ec555bb18024a46956ca0
yuto ichinose <yuto.0526.sano@gmail.com>
2022-05-09 21:33:32 +0900

    ニュース記事一覧とニュース記事を切り替える画面

--- App.js
+++ App.js
@@ -1,43 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { FlatList, StyleSheet, View, } from "react-native";
-import NewsArticle from "./components/NewsArticle";
-import Counter from "./components/Counter";
-import Constants from "expo-constants";
-import axios from "axios";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import NewsScreen from "./screens/NewsScreen";
+import DetailScreen from "./screens/DetailScreen";
 
-const APIKey = Constants.manifest.extra.newsApiKey
-const URI = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${APIKey}`
+const Stack = createStackNavigator()
 
-export default function App() {
-  const [news,setNews] = useState([])
-  useEffect(()=>{
-    getNews()
-  },[])
-
-  const getNews = async () => {
-    const response = await axios.get(URI)
-    setNews(response.data.articles);
-  }
+export default App = () => {
   return (
-    <View style={styles.container}>
-      <FlatList
-        data = {news}
-        renderItem = {({item}) => (
-          <NewsArticle 
-            imageuri = {item.urlToImage} 
-            title = {item.title} 
-            subText = {item.publishedAt}
-          />
-        )}
-        keyExtractor = {(item, index) => index.toString()}
-      />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = 'News' component={NewsScreen}/>
+        <Stack.Screen name = '詳細ページ' component={DetailScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
